ecc8b5d88201c69865d74b4bf936058544ec0351
朱佳伟 <7392034+jia_j_w@user.noreply.gitee.com>
2020-07-12 11:33:29 +0800

    新增登录页面

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button, message } from 'antd'
+import { HashRouter, Route, Router, Switch } from 'react-router-dom'
+import './style/index.less'
+import Login from './pages/login/login'
+import Admin from './pages/admin/admin'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrerhhh"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component {
+
+
+  render() {
+    return (
+      <HashRouter>
+        <Switch>
+          <Route path='/login' component={ Login } />
+          <Route path='/' component={ Admin } />
+        </Switch>
+      </HashRouter>
+    )
+ }
 }
 
 export default App;
---
+++ src/pages/login/login.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import './login.less'
+import logo from '../../images/logo192.png'
+import LoginForm from './LoginForm/LoginForm'
+
+export default class Login extends Component{
+
+    render() {
+        return (
+            <div className='login'>
+                <div className='login-header pos-rel'>
+                    <img src={logo} alt="" />
+                    <h3 className='white'>React 后台管理系统</h3>
+                    <div className='pos-abs w-100 h-100  modal'></div>
+                </div>
+                <div className='login-content contentFlex'>
+                    <LoginForm />
+                </div>
+            </div>
+           
+        )
+    }
+}
